391aa16b301f736e1323c725400bf06aa5cac765
Rhawan Brenner Silva Franco <rhawan-bs@detran.go.gov.br>
2019-04-17 15:20:26 -0300

    Optmizing

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -31,7 +31,7 @@ export default class Box extends Component {
     io.emit("connectRoom", box);
 
     io.on("file", data => {
-        this.setState({ box: { ... this.state.box, files: [data, ... this.state.box.files ] } })
+        this.setState({ box: { ...this.state.box, files: [data, ...this.state.box.files ] } })
     })
   }
 
